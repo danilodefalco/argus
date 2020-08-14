724b16452e6630e29a0563528cd578b9f7b496a4
Danilo <danilo@4cadia.com>
2020-08-14 19:18:48 -0300

    Argus Single Page
    Argus Single Page

--- argus/src/app/app.module.ts
+++ argus/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './navbar/navbar.component';
+import { ArgusComponent } from './argus/argus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavBarComponent,
+    ArgusComponent
   ],
   imports: [
     BrowserModule,
---
+++ argus/src/app/argus/argus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-argus',
+  templateUrl: './argus.component.html',
+  styleUrls: ['./argus.component.css']
+})
+export class ArgusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
